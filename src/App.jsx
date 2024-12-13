7b16316df80cba65f653568a32bbb0b81aeee47c
Chapunk1620 <jhonchristiansolano@gmail.com>
2024-12-13 10:25:11 +0800

    asdfw

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import TodoList from './components/TodoList' 
 import TodoInput from './components/TodoInput' // Assuming TodoInput is defined in TodoInput.jsx
-import {useState} from "react"
+import {useState, useEffect} from "react"
 
 function App() {
 
   const [todos, setTodos] = useState([])
   const [todoValue, setTodoValue] = useState('')
 
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList}))
+  }
+
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo]
+    persistData(newTodoList)
     setTodos(newTodoList)
   }
 
@@ -16,6 +21,7 @@ function App() {
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index
     })
+    persistData(newTodoList)
     setTodos(newTodoList)
   }
 
@@ -24,6 +30,20 @@ function App() {
     setTodoValue(valueToBeEdited)
     handleDeleteTodo(index)
   }
+
+  useEffect(() => {
+    if (!localStorage){
+      return
+    }
+    
+    let localTodos = localStorage.getItem('todos')
+
+    if (!localTodos) {
+      return
+    }
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+  }, [])
 
   return (
     <>
